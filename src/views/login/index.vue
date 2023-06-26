9a68a7944609c18bbc774e4f60499b1bd5696692
梁惠涌 <1026946613@qq.com>
2023-06-26 22:59:52 +0800

    up

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,14 +1,6 @@
 <template>
-  <div class="test-layout flex-center-center">
+  <div class="page-login flex-center-center">
     <div class="flex-center-center" style="flex-direction: column;">
-      <div class="flex">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://vuejs.org/" target="_blank">
-          <img src="../../assets/vue.svg" class="logo vue" alt="Vue logo" />
-        </a>
-      </div>
       <h1>Login</h1>
     </div>
   </div>
