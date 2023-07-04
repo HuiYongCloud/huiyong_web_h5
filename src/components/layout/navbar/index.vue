2b4410082ec230a6f21e1de6e84322d86edc7be1
梁惠涌 <1026946613@qq.com>
2023-07-04 22:47:51 +0800

    up

--- src/components/layout/navbar/index.vue
+++ src/components/layout/navbar/index.vue
@@ -2,40 +2,45 @@
 	<div class="layout-nav-bar flex-center-end">
 		<ThemeSwitch/>
 		<div class="ml20">
-			<Popover v-model:show="showPopover" :actions="actions" @select="onSelect">
-				<template #reference>
-					<div class="flex-center-center">
-						<img class="user-header " :src="userInfo.userImage">
-						<div class="user-name ml5">{{userInfo.userName}}</div>
-						<Icon class="ml5" name="arrow-down" />
-					</div>
-				</template>
-			</Popover>
+			<div class="flex-center-center" @mouseover="state.isSelectUser = true" @mouseleave="state.isSelectUser = false">
+				<img class="user-header" :src="userInfo.userImage">
+				<div class="user-name ml5">{{userInfo.userName}}</div>
+				<Icon class="ml5" name="arrow-down" />
+			</div>
+			<div class="user-menu-box">
+				<transition name="slide-top">
+					<div class="user-menu-list" v-if="state.isSelectUser">
+						<div class="user-info flex-center-start">
+							<div class="menu-user-image">
+								<img class="user-header" :src="userInfo.userImage"/>
+							</div>
+							<div class="menu-user-name">{{userInfo.userName}}</div>
+						</div>
+						<div class="menu-item flex-center-between">首页</div>
+						<div class="menu-line"/>
+						<div class="menu-item flex-center-between">
+							<div>退出登录</div>
+						</div>
+					</div>  
+				</transition>
+			</div>
 		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
-import { defineAsyncComponent, ref} from 'vue';
+import { defineAsyncComponent, reactive, ref} from 'vue';
 import { appStore } from '/@/stores/appStore'
 import { Icon } from 'vant';
-import { Popover } from 'vant';
 
 // 定义变量
 const mainStore = appStore()
 const userInfo = mainStore.userInfo
-const showPopover = ref(false);
-
+const state = reactive({
+	isSelectUser: false
+});
 // 引入组件
 const ThemeSwitch = defineAsyncComponent(() => import('/@/components/theme-switch/index.vue'));
-const actions = [
-	{ text: '首页', type: 0 },
-	{ text: '退出登录', type: 1 },
-];
-const onSelect = (action: any) => {
-	console.log(action)
-}
-
 </script>
 
 <style scoped lang="scss">
@@ -51,6 +56,94 @@ const onSelect = (action: any) => {
 
 	.user-name{
 		font-size: 14px;
+	}
+
+	.user-menu-box{
+		position: absolute;
+		z-index: 1;
+		right: 0px;
+		top: 0px;
+		margin-top: 50px;
+		overflow: hidden;
+	}
+
+	.user-menu-list{
+		width: 160px;
+		padding: 4px 0;
+		box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.12), 0 0.5px 1.5px 0 rgba(0, 0, 0, 0.08);
+		background: white;
+		border-radius: 3px;
+		overflow: hidden;
+		margin: 2px 16px 16px;
+
+		.user-info{
+			padding: 0 16px;
+			margin: 16px 0;
+
+			.menu-user-image{
+				width: 36px;
+			}
+
+			.menu-user-name{
+				margin-left: 10px;
+				color: #202d40;
+				font-size: 15px;
+				white-space: nowrap;
+				overflow: hidden;
+				text-overflow: ellipsis;
+			}
+		}
+
+		.menu-item{
+			line-height: 32px;
+			height: 32px;
+			padding: 0 16px;
+			color: #202d40;
+			font-size: 14px;
+			transition: all .3s ease;
+			cursor: pointer;
+
+			&:hover{
+				background: #f5f7fa;
+			}
+
+			.switch-btn{
+				margin-right: -10px;
+				transform: scale(.6);
+			}
+
+			.menu-svg{
+				color: #bdc0c6;
+			}
+		}
+
+		.menu-line{
+			height: 1px;
+			background-color: #000;
+			opacity: .04;
+			margin: 4px 0;
+		}
+	}
+
+	.slide-top-enter-active {
+		animation: slide-top-active .25s ease;
+	}
+	.slide-top-leave-active {
+		animation: slide-top-leave .25s ease;
+	}
+
+	@keyframes slide-top-active {
+		0% {
+			transform: translateY(-100%);
+		}
+		100% {
+			transform: translateY(0);
+		}
+	}
+	@keyframes slide-top-leave {
+		100% {
+			transform: translateY(-100%);
+		}
 	}
 }
 </style>
